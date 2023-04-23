7ed87bccf80a6e54f8d221c7550e622652d04f27
moaazsalama <moaazsalama6@gmail.com>
2023-04-23 15:35:32 +0200

    update

---
+++ coments.dart
@@ -0,0 +1,9 @@
+// JavaScript not Object Oriented
+// Javascript have no interface
+// javascript have no inheritance
+// SOLID
+// S - Single Responsibility Principle
+// O - Open Closed Principle
+// L - Liskov Substitution Principle
+// I - Interface Segregation Principle
+// D - Dependency Inversion Principle
